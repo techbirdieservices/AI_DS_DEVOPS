2e4335783009b116c464b47f30b8beb4bf2c31e9
t0xicm4ddy99-cmd <t0xicm4ddy99@gmail.com>
2025-08-30 08:36:54 +0530

    create

---
+++ sum.py
@@ -0,0 +1,22 @@
+a=int(input("Enter the Value:"))
+b=int(input("Enter the Value:"))
+subt=a-b
+print("The Sub is:",subt)
+
+
+
+
+
+*********************************************************
+
+
+a=int(input("Enter the Value:"))
+b=int(input("Enter the Value:"))
+mult=a*b
+print("The Multiply is:",mult)
+
+
+
+
+
+*********************************************************
