7819262ede11fe760de83f6753eeb4526dfafe23
t0xicm4ddy99-cmd <t0xicm4ddy99@gmail.com>
2025-08-30 08:30:27 +0530

    main

--- user.py
+++ user.py
@@ -2,3 +2,16 @@ a=int(input("Enter the Value:"))
 b=int(input("Enter the Value:"))
 sum=a+b
 print("The Sum is:",sum)
+
+
+
+
+
+*********************************************************
+
+
+
+p=int(input("Enter the Value:"))
+q=int(input("Enter the Value:"))
+sum=p/q
+print("The Correct Ans is:",sum)
